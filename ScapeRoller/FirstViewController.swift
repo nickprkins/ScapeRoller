d66a00e8d1b5a3371a0fedf9f9253a3a3046d7c4
Nick Perkins <nickperkins@protonmail.com>
2016-05-25 16:02:14 -0400

    initial commit, added UI and CollectionViews

--- ScapeRoller/FirstViewController.swift
+++ ScapeRoller/FirstViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
+
+// Responsive UIView Extension for Background Image
+extension UIView {
+    func addBackground() {
+        // screen width and height:
+        let width = UIScreen.mainScreen().bounds.size.width
+        let height = UIScreen.mainScreen().bounds.size.height
+        
+        let imageViewBackground = UIImageView(frame: CGRectMake(0, 0, width, height))
+        imageViewBackground.image = UIImage(named: "scapeBackground")
+        
+        // you can change the content mode:
+        imageViewBackground.contentMode = UIViewContentMode.ScaleAspectFill
+        
+        self.addSubview(imageViewBackground)
+        self.sendSubviewToBack(imageViewBackground)
+    }}
+
 class FirstViewController: UIViewController {
 
+    
+    @IBOutlet weak var attackCollectionView: UICollectionView!
+    
+    @IBOutlet weak var defenseCollectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // call responsive background UIView Extension
+        self.view.addBackground()
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func addAttackButtonPressed(sender: AnyObject) {
+        
+        
+        
     }
-
-
+    
+    @IBAction func subtractAttackButtonPressed(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        if collectionView == self.attackCollectionView {
+            
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("attackCell", forIndexPath: indexPath) as UICollectionViewCell
+            
+            return cell
+            
+        } else { // do not do this check: if collectionView == self.hourCollectionView {
+            
+//            let cell: = collectionView.dequeueReusableCellWithReuseIdentifier("defenseCell", forIndexPath: indexPath) as UICollectionViewCell
+//            
+//            
+//            return cell
+        }
+    }
+    
 }
-
